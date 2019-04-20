1e7d44399423a2c196fe1f6acce30bb17c057cec
isaachiroman <isaachiroman@gmail.com>
2019-04-20 20:35:03 +0700

    Updates

---
+++ precache-manifest.dd00e27f929d6339504805a94cc56030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d83acb329fbe1a7b3365",
+    "url": "/nhansu/static/js/runtime~main.1a083b79.js"
+  },
+  {
+    "revision": "d3a933fc5bb57d78b6b4",
+    "url": "/nhansu/static/js/main.fa5dc554.chunk.js"
+  },
+  {
+    "revision": "5c85c970680b706dc104",
+    "url": "/nhansu/static/js/2.74a1c92f.chunk.js"
+  },
+  {
+    "revision": "d3a933fc5bb57d78b6b4",
+    "url": "/nhansu/static/css/main.1706b478.chunk.css"
+  },
+  {
+    "revision": "9ddd60b7b25f24fe2a753ed9ea6eb36c",
+    "url": "/nhansu/index.html"
+  }
+];
